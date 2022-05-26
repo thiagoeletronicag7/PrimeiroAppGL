3ec0692d7c01265706b1a71384210b51db940fcc
TunelessBat <thiagoeletronicag7@gmail.com>
2022-05-26 18:07:56 -0300

    tiro em forma de lazer

--- src/primeiroApp/Main.java
+++ src/primeiroApp/Main.java
@@ -16,6 +16,33 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main
 {
+
+    public static void tiro(float centroVerticeEsquerdoX, float centroVerticeDireitoX, boolean inicia)
+    {
+        float y = 0;
+
+       while(inicia)
+        {
+            glBegin(GL_QUADS);
+            glColor4f(0, 1.0f, 0,0);
+            glVertex2f(centroVerticeEsquerdoX, -0.6f + y);
+
+            glColor4f(1.0f, 0, 0,0);
+            glVertex2f(centroVerticeDireitoX, -0.6f + y);
+
+            glColor4f(1.0f, 0, 0,0);
+            glVertex2f(centroVerticeDireitoX, -0.5f + y);
+
+            glColor4f(0, 1.0f, 0,0);
+            glVertex2f(centroVerticeEsquerdoX, -0.5f + y);
+            glEnd();
+
+            y = y + 0.1f;
+
+            if(y >= 2) break;
+        }
+
+    }
     public Main()
     {
         //inicializa o gl e testa
@@ -37,7 +64,10 @@ public class Main
         //cria o contexto
         GL.createCapabilities();
 
+        //coordenadas
         float x = 0;
+        float y = 0;
+        //variaveis de controle de cor
         float color_red = 1;
         float color_blue = 0;
 
@@ -46,23 +76,30 @@ public class Main
         {
             if(glfwGetKey(janela, GLFW_KEY_D) == GL_TRUE)
             {
-                System.out.println(x += 0.001f);
+                x += 0.001f;
+                if(y >= 0 && y <= 1)
+                {
+                    y += 0.001f;
+                } else
+                {
+                    y -= 0.001f;
+                }
+
+
             }
             if(glfwGetKey(janela, GLFW_KEY_A) == GL_TRUE)
             {
-                System.out.println(x -= 0.001f);
-            }
-            if(glfwGetKey(janela, GLFW_KEY_SPACE) == GL_TRUE)
-            {
-                color_red = 0;
-                color_blue = 1;
-            } else
-            {
-                color_red = 1;
-                color_blue = 0;
+                x -= 0.001f;
+                y -= 0.001f;
             }
 
+
             glfwPollEvents();
+
+            //variáveis que serão passadas como coordenadas para os parametros do tiro
+            float centroVerticeEsquerdoX = -0.1f + x;
+            float centroVerticeDireitoX = 0.1f + x;
+
 
             //limpa o contexto
             glClear(GL_COLOR_BUFFER_BIT);
@@ -70,26 +107,40 @@ public class Main
             //desenha um quadrado
             glBegin(GL_QUADS);
                 glColor4f(color_red, 1.0f, color_blue,0);
-                glVertex2f(-0.5f+x, 0.5f);
+                glVertex2f(centroVerticeEsquerdoX, -0.8f);
 
                 glColor4f(color_red, 0, color_blue,0);
-                glVertex2f(0.5f+x, 0.5f);
+                glVertex2f(centroVerticeDireitoX, -0.8f);
 
                 glColor4f(color_red, 0, color_blue,0);
-                glVertex2f(0.5f+x, -0.5f);
+                glVertex2f(centroVerticeDireitoX, -0.6f);
 
                 glColor4f(color_red, 1.0f, color_blue,0);
-                glVertex2f(-0.5f+x, -0.5f);
+                glVertex2f(centroVerticeEsquerdoX, -0.6f);
             glEnd();
+
+            if(glfwGetKey(janela, GLFW_KEY_SPACE) == GL_TRUE)
+            {
+               /* color_red = 0;
+                color_blue = 1;*/
+                tiro(centroVerticeEsquerdoX, centroVerticeDireitoX, true);
+
+            }
+
+            //tiro(centroVerticeEsquerdoX, centroVerticeDireitoX);
 
             //faz a troca de buffers
             glfwSwapBuffers(janela);
         }
 
+
         glfwTerminate();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         new Main();
+
     }
 }
